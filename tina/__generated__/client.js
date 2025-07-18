135defb72052852e425d7a9863095ce6d2eae366
Sam Prestigiacomo <samrprestigiacomo@gmail.com>
2025-07-17 22:10:34 -0500

    Implement smart TinaCMS build with fallback - maintains Step 2 completion

--- tina/__generated__/client.js
+++
@@ -1,4 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ cacheDir: "/Users/sampresto/Desktop/iamlookingforvintage/tina/__generated__/.cache/1752806774983", url: "http://localhost:4001/graphql", token: "230aea76572b81944484dd66a4fb2377a1b63e0d", queries });
-export default client;
